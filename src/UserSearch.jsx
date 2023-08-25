db3745063e12c95e140463e7ba472c0cffa9d3cb
jf7689 <jf7689@rit.edu>
2023-08-25 18:53:49 -0400

    Initial page search bar functions and unfinished code to fetch from twitch api

---
+++ src/UserSearch.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react"
+
+export function UserSearch() {
+    const [newName, setNewName] = useState("")
+    function handleSubmit(e) {
+        e.preventDefault()
+    
+        fetch(
+            'https://api.twitch.tv/helix/users?id=' + twitch_id,
+            {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Client-ID': client_id
+                }
+            }
+        ).then(response => response.json()
+        ).then(response => {
+        }).catch(error => {
+            // log issue
+            console.log(error);
+        });
+    
+        setNewName("")
+    }
+
+    return (
+        <form onSubmit={handleSubmit} className="streamer-form">
+        <div className="form-row">
+          <input value={newName} onChange={e => e.target.value} type="text" id="user-search" placeholder="Twitch Username" />
+        </div>
+        <button className="btn">Search</button>
+      </form>
+    )
+}
