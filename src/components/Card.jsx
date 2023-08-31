cd87c3fd10c9c7b897016dda1983e678d0d23b02
jf7689 <jf7689@rit.edu>
2023-08-31 18:10:52 -0400

    Separate card into it's own component and css

---
+++ src/components/Card.jsx
@@ -0,0 +1,27 @@
+import styles from "../assets/styles/card.module.css";
+
+export function Card({url, thumbnail, title, views, date, duration}) {
+    return (
+        <div className={styles.card}>
+            <a target="_blank" href={`${url}`} rel="noopener noreferrer">
+                <div>
+                    <div>
+                        <img className={`${styles.img} ${styles.card}`} src={`${thumbnail}`} alt="Thumbnail"/>
+                    </div>
+                    <h3 className={styles.title}>{title}</h3>
+                    <div>
+                        <div>
+                            {views} views
+                        </div>
+                        <div>
+                            {date}
+                        </div>
+                        <div>
+                            {duration}
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+    );
+}
--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
-import styles from "../assets/grid.module.css";
+import { Card } from "./Card";
+import styles from "../assets/styles/grid.module.css";
+import liveIcon from "../assets/images/live.png";
 
 export function Grid({url, client_id, token, streamer_id}) {
     const [vods, setVods] = useState([]);
@@ -28,43 +30,23 @@ export function Grid({url, client_id, token, streamer_id}) {
         });
     }, [streamer_id]);
 
-    function checkVods() {
-        console.log(vods);
-        console.log(streamer_id);
-    }
-
     return (
         <div className={styles.grid}>
         {vods.map(vod => {
             // set thumbnail dimensions for url
             let thumbnail = vod.thumbnail_url;
-            thumbnail = thumbnail.replace("%{width}", "666");
-            thumbnail = thumbnail.replace("%{height}", "375");
-            console.log(thumbnail);
+            thumbnail = thumbnail.replace("%{width}", "666").replace("%{height}", "375");
+            if (thumbnail.includes("404/404")) {
+                thumbnail = liveIcon;
+            }
 
             // Create Grid cards
             return (
-                <div className={styles.card} key={vod.id}>
-                    <a target="_blank" href={`${vod.url}`} rel="noopener noreferrer">
-                        <div>
-                            <div>
-                                <img className={styles.img} src={`${thumbnail}`} alt="Thumbnail"/>
-                            </div>
-                            <h3 className={styles.title}>{vod.title}</h3>
-                            <div>
-                                <div>
-                                    {formatter.format(vod.view_count)} views
-                                </div>
-                                <div>
-                                    {vod.published_at.slice(0,10)}
-                                </div>
-                                <div>
-                                    {vod.duration}
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
+                
+                <Card key={vod.id} url={vod.url} thumbnail={thumbnail} title={vod.title} views={formatter.format(vod.view_count)}
+                date={vod.published_at.slice(0,10)} duration={vod.duration}
+                />
+                
             );
         })}
         </div>
