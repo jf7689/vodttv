dbfb30648ccd82e2b5a6a8dd4da2202af5cd7cee
jf7689 <jf7689@rit.edu>
2023-09-05 18:23:23 -0400

    Working on infinite scroll with intersection observer

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -5,8 +5,11 @@ import liveIcon from "../assets/images/live.png";
 
 export function Grid({url, client_id, token, streamer_id}) {
     const [vods, setVods] = useState([]);
+    const [allVods, setAllVods] = useState([]);
+    const [vodStart, setVodStart] = useState(0);
+    const [vodEnd, setVodEnd] = useState(30);
     const formatter = Intl.NumberFormat("en", { notation: "compact" });
-    let allVods = [];
+    let throttleTimer;
 
     // Get vods
     async function getVods() {
@@ -17,7 +20,7 @@ export function Grid({url, client_id, token, streamer_id}) {
             try {
                 // Get first page of vods
                 const response = await fetch(
-                    `${url}/videos?user_id=${streamer_id}&type=archive&first=100`,
+                    `${url}/videos?user_id=${streamer_id}&type=archive&first=30`,
                     {
                         method: "GET",
                         headers: {
@@ -27,7 +30,14 @@ export function Grid({url, client_id, token, streamer_id}) {
                     }
                 )
                 const responseData = await response.json();
-                allVods.push(...responseData.data);
+
+                // Store vods
+                setVods(responseData.data);
+                setAllVods(currentVods => {
+                    return [...currentVods, ...responseData.data]
+                });
+                
+                // Check for next page of vods
                 paginationObj = responseData.pagination;
                 if (Object.keys(paginationObj).length !== 0) {
                     cursor = paginationObj.cursor;
@@ -52,7 +62,13 @@ export function Grid({url, client_id, token, streamer_id}) {
                         }
                     )
                     const responseData = await response.json();
-                    allVods.push(...responseData.data);
+
+                    // Store vods
+                    setAllVods(currentVods => {
+                        return [...currentVods, ...responseData.data]
+                    });
+
+                    // Check for next page of vods
                     paginationObj = responseData.pagination;
                     if (Object.keys(paginationObj).length !== 0) {
                         cursor = paginationObj.cursor;
@@ -62,18 +78,53 @@ export function Grid({url, client_id, token, streamer_id}) {
                     console.log(error);
                 }
             }
-            
-            setVods(allVods);
         }
     }
+
+    function loadNewCards() {
+        setVodStart(vodEnd);
+        setVodEnd(vodStart + 30);
+        setVods(currentVods => {
+            return [...currentVods, ...allVods.slice(vodStart, vodEnd)]
+        });
+        console.log(vodStart);
+        console.log(vodEnd);
+    }
     
+    // Username had been searched
     useEffect(() => {
         getVods();
+        console.log("Grab Vods");
+
     }, [streamer_id]);
+
+    /*useEffect(() => {
+        const observer = new IntersectionObserver(entries => {
+            const lastCard = entries[0];
+            if (!lastCard.isIntersecting) return;
+            loadNewCards();
+            observer.unobserve(lastCard.target);
+            observer.observe(vods[vods.length - 1])
+        }, {
+            rootMargin: "100px"
+        });
+    
+        observer.observe(vods[vods.length - 1]);
+        console.log("Load vods");
+
+    }, [vods]);*/
+
+    function checkVods() {
+        console.log(allVods);
+        console.log(vods);
+    }
 
     return (
         <div className={styles.grid}>
-        {console.log(vods)}
+        <button onClick={checkVods}>Vods</button>
+        {
+            
+        }
         {vods.map(vod => {
             // set thumbnail dimensions for url
             let thumbnail = vod.thumbnail_url;
@@ -84,11 +135,9 @@ export function Grid({url, client_id, token, streamer_id}) {
 
             // Create Grid cards
             return (
-                
                 <Card key={vod.id} url={vod.url} thumbnail={thumbnail} title={vod.title} views={formatter.format(vod.view_count)}
                 date={vod.published_at.slice(0,10)} duration={vod.duration}
                 />
-                
             );
         })}
         </div>
