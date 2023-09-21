b587373b258c1cf6a7c78c6781e6662f36c9f1a4
jf7689 <jf7689@rit.edu>
2023-09-21 16:48:30 -0400

    Align views and upload date to bottom of card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,21 +2,21 @@ import styles from "../assets/styles/card.module.css";
 
 export function Card({url, thumbnail, title, views, date, duration}) {
     return (
-            <div className={styles.card}>
-                <a target="_blank" href={`${url}`} rel="noopener noreferrer">
-                    <div>
+            <div className={styles.cardContainer}>
+                <a className={styles.card} target="_blank" href={`${url}`} rel="noopener noreferrer">
+                    <div className={styles.contentContainer}>
                         <div className={styles.thumbnailContainer}>
-                            <img className={`${styles.thumbnail}`} src={`${thumbnail}`} alt="Thumbnail"/>
+                            <img className={styles.thumbnail} src={thumbnail} alt="Thumbnail"/>
                             <div className={styles.durationContainer}>
                                 {duration}
                             </div>
                         </div>
                         <h3 className={styles.title}>{title}</h3>
-                        <div>
-                            <div>
+                        <div className={styles.infoContainer}>
+                            <div className={styles.infoItem}>
                                 {views} views
                             </div>
-                            <div>
+                            <div className={styles.infoItem}>
                                 {date}
                             </div>
                         </div>
