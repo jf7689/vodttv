40596de1ad6ba74693d2dacfb0b34be39427387a
jf7689 <jf7689@rit.edu>
2023-09-22 16:07:53 -0400

    Prioritize header on top layer. Set modal width and backdrop color

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -258,29 +258,31 @@ export function Grid({url, client_id, token, streamer_id}) {
                     </div>
                 </form>
                 <dialog className={styles.dialog} id="dateFilter">
-                    <select value={year} onChange={handleSetYear}>
-                        <option value="Year">Year</option>
-                        {allYears.map(yearOption => {
-                            return (
-                            <option key={yearOption} value={yearOption}>{yearOption}</option>
-                            );
-                        })}
-                    </select>
-                    <select value={month} onChange={handleSetMonth}>
-                        <option value="Month">Month</option>
-                        <option value="01">January</option>
-                        <option value="02">February</option>
-                        <option value="03">March</option>
-                        <option value="04">April</option>
-                        <option value="05">May</option>
-                        <option value="06">June</option>
-                        <option value="07">July</option>
-                        <option value="08">August</option>
-                        <option value="09">September</option>
-                        <option value="10">October</option>
-                        <option value="11">November</option>
-                        <option value="12">December</option>
-                    </select>
+                    <div className={styles.dropdownContainer}>
+                        <select value={year} onChange={handleSetYear}>
+                            <option value="Year">Year</option>
+                            {allYears.map(yearOption => {
+                                return (
+                                <option key={yearOption} value={yearOption}>{yearOption}</option>
+                                );
+                            })}
+                        </select>
+                        <select value={month} onChange={handleSetMonth}>
+                            <option value="Month">Month</option>
+                            <option value="01">January</option>
+                            <option value="02">February</option>
+                            <option value="03">March</option>
+                            <option value="04">April</option>
+                            <option value="05">May</option>
+                            <option value="06">June</option>
+                            <option value="07">July</option>
+                            <option value="08">August</option>
+                            <option value="09">September</option>
+                            <option value="10">October</option>
+                            <option value="11">November</option>
+                            <option value="12">December</option>
+                        </select>
+                    </div>
                     <div className={styles.btnFilterContainer}>
                         <button className={styles.dialogBtn} onClick={dateFilter}>Filter</button>
                         <button className={styles.dialogBtn} onClick={closeFilterModal}>Close</button>
